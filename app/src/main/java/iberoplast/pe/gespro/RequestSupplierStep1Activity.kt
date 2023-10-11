36b2a6b03c5e2f8ba282bacd0c5b5014693b68ae
ALEXIS YUNIOR CURO PONCE <acurop@autonoma.edu.pe>
2023-10-11 13:32:52 -0500

    Retorno de fases del formulario y validacion de algunos campos

--- app/src/main/java/iberoplast/pe/gespro/RequestSupplierStep1Activity.kt
+++ app/src/main/java/iberoplast/pe/gespro/RequestSupplierStep1Activity.kt
@@ -3,6 +3,7 @@ package iberoplast.pe.gespro
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,11 +11,23 @@ class RequestSupplierStep1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_request_supplier_step1)
-        val btnFormRequest1 = findViewById<Button>(R.id.btnFormRequest1)
+        val btnNext = findViewById<Button>(R.id.btnNextFormRequest1)
 
-        btnFormRequest1.setOnClickListener{
-            val intent = Intent(this, RequestSupplierStep2Activity::class.java)
-            startActivity(intent)
+        btnNext.setOnClickListener{
+            val etNicRuc = findViewById<EditText>(R.id.etNicRuc)
+            val nicRucText = etNicRuc.text.toString().trim()
+
+            if (nicRucText.isEmpty()) {
+                etNicRuc.error = "El NIC o RUC no puede estar vacío"
+            } else if (!nicRucText.matches(Regex("^[0-9]+$"))) {
+                etNicRuc.error = "Solo se permiten números en el NIC o RUC"
+            } else if (nicRucText.length < 10) {
+                etNicRuc.error = "El NIC o RUC es demasiado corto"
+            } else {
+                // Continuar con el paso 2
+                val intent = Intent(this, RequestSupplierStep2Activity::class.java)
+                startActivity(intent)
+            }
         }
     }
 
